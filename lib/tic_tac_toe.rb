2b515d4a9bc8ebbbf3790f412d47e964ec10b642
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-14 06:01:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,7 +33,7 @@ class TicTacToe
   end
   
   def position_taken?(index)
-    if @board[index] == "X" || @board[index] "O"
+    if @board[index] == "X" || @board[index] == "O"
       true
     else
       false
