3551d8d76c35e2fb1761c4efe65ed67ebadc2a38
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-14 06:22:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,13 +46,15 @@ end
   
   def turn_count
     counter = 0
-    #@board.each do |el|
+    @board.each do |el|
       if el == "X" || el == "O"
     counter += 1
   end
 end
   counter
   end
+  
+  def current_player
   
     
 end
